228523ba4b6c90113465bd5a2b6478bf6b78d264
김대현 <seo223kr@naver.com>
2021-12-14 17:45:36 +0900

    마무리
    gm은 테스트용으로 바뀜

--- Source/PortFolio/CPP_MainGM.cpp
+++ Source/PortFolio/CPP_MainGM.cpp
@@ -8,8 +8,8 @@
 
 ACPP_MainGM::ACPP_MainGM() {
 	
-	DefaultPawnClass = ConstructorHelpers::FClassFinder<ABaseCharacter>(TEXT("Blueprint'/Game/BluePrint/Character/BP_Character.BP_Character_C'")).Class;
+	DefaultPawnClass = ConstructorHelpers::FClassFinder<ABaseCharacter>(TEXT("Blueprint'/Game/BluePrint/Character/BP_Character'")).Class;
 	
-	PlayerControllerClass = ConstructorHelpers::FClassFinder<ACPP_CharacterController>(TEXT("Blueprint'/Game/BluePrint/Character/PlayerController.PlayerController_C'")).Class;
+	PlayerControllerClass = ConstructorHelpers::FClassFinder<ACPP_CharacterController>(TEXT("Blueprint'/Game/BluePrint/Character/PlayerController'")).Class;
 
 }
